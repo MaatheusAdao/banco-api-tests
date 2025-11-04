6d23dabb0a576b2fc71f8bf0a4bba861f9cfe5c6
Matheus Adão <matheusguilhermeadao@gmail.com>
2025-11-04 10:28:15 -0300

    Criei um helper de autenticação para obter o token

--- test/transferencias.test.js
+++ test/transferencias.test.js
@@ -2,21 +2,13 @@ import request from 'supertest';
 import {expect} from 'chai';
 import dotenv from 'dotenv';
 dotenv.config();
-
+import { obterToken } from '../helpers/autenticacao.js';
 
 describe('Tranferencias', () => {
     describe('POST/tranferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor for maior ou que R$:10,00 reias',async () => {
-            //Capturando  o token
-            const respostaLogin = await request(process.env.BASE_URL)
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-                    })
-
-            const token = respostaLogin.body.token
+          
+            const token = await obterToken ('julio.lima','123456')
             
 
             const resposta = await request(process.env.BASE_URL)
@@ -35,18 +27,11 @@ describe('Tranferencias', () => {
         })
 
          it('Deve retornar fracaso com 422 quando o valor for menor que R$:10,00 reias',async  () => {
-            const respostaLogin = await request('http://localhost:3000')
-                .post('/login')
-                .set('Content-Type', 'application/json')
-                .send({
-                    'username': 'julio.lima',
-                    'senha': '123456'
-                    })
-
-            const token = respostaLogin.body.token
+    
+            const token = await obterToken ('julio.lima','123456')
             
 
-            const resposta = await request('http://localhost:3000')
+            const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set ('Authorization',`Bearer ${token}`)
